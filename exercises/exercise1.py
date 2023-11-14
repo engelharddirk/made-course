7ebd4ab5b8572b3d03e8811f06d3827d94637844
DerDirktator <65366713+DerDirktator@users.noreply.github.com>
2023-11-14 14:10:33 +0100

    remove auto-added index column

--- exercises/exercise1.py
+++ exercises/exercise1.py
@@ -1,6 +1,6 @@
 import sqlalchemy
 import pandas as pd
 
-dataframe = pd.read_csv("https://opendata.rhein-kreis-neuss.de/api/v2/catalog/datasets/rhein-kreis-neuss-flughafen-weltweit/exports/csv",";")
+dataframe = pd.read_csv("https://opendata.rhein-kreis-neuss.de/api/v2/catalog/datasets/rhein-kreis-neuss-flughafen-weltweit/exports/csv",";",)
 engine = sqlalchemy.create_engine("sqlite:///airports.sqlite")
-dataframe.to_sql("airports", engine)
+dataframe.to_sql("airports", engine, index=False)
