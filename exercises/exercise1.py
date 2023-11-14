93b5c1d85ea47d3b4aa2b5271f6c0b3438822341
DerDirktator <65366713+DerDirktator@users.noreply.github.com>
2023-11-14 14:15:44 +0100

    style changes

--- exercises/exercise1.py
+++ exercises/exercise1.py
@@ -1,6 +1,6 @@
-import sqlalchemy
+import sqlalchemy as sal
 import pandas as pd
 
-dataframe = pd.read_csv("https://opendata.rhein-kreis-neuss.de/api/v2/catalog/datasets/rhein-kreis-neuss-flughafen-weltweit/exports/csv",";",)
-engine = sqlalchemy.create_engine("sqlite:///airports.sqlite")
+dataframe = pd.read_csv("https://opendata.rhein-kreis-neuss.de/api/v2/catalog/datasets/rhein-kreis-neuss-flughafen-weltweit/exports/csv",";")
+engine = sal.create_engine("sqlite:///airports.sqlite")
 dataframe.to_sql("airports", engine, index=False)
