e98d27e7bcd8791895bcefbc636db9f8be786f9f
DerDirktator <65366713+DerDirktator@users.noreply.github.com>
2023-11-21 23:55:09 +0100

    clean retrieved data, refactoring

--- project/pipeline.py
+++ project/pipeline.py
@@ -2,8 +2,35 @@ import sqlalchemy as sal
 from sqlalchemy import BIGINT, TEXT, FLOAT
 import pandas as pd
 
-dataframe_satisfaction = pd.read_csv("https://ec.europa.eu/eurostat/api/dissemination/sdmx/2.1/data/ilc_pw01/?format=TSV&compressed=false","\t")
-engine = sal.create_engine("sqlite:///data/projectdata.sqlite")
-dataframe_satisfaction.to_sql("satisfaction", engine, index=False, if_exists="replace")
-dataframe_movies = pd.read_csv("https://datasets.imdbws.com/title.basics.tsv.gz","\t")
-dataframe_movies.to_sql("movies", engine, index=False, if_exists="replace")
+FILEPATH = "/data/projectdata.sqlite"
+SATISFACTION_SOURCE = "https://ec.europa.eu/eurostat/api/dissemination/sdmx/2.1/data/ilc_pw01/?format=TSV&compressed=false"
+MOVIES_SOURCE = "https://datasets.imdbws.com/title.basics.tsv.gz"
+
+def prepare_pipeline_engine():
+    engine = sal.create_engine(f"sqlite://{FILEPATH}")
+    return engine
+
+def get_dataframes_from_sources():
+    df_list = [
+        pd.read_csv(SATISFACTION_SOURCE, "\t|,"),
+        pd.read_csv(MOVIES_SOURCE, "\t")
+    ]
+    return df_list
+
+def process_dataframes(dataframes):
+    df_satisfaction_only_lifesat = dataframes[0][dataframes[0]["indic_wb"].str.contains("LIFESAT")]
+    df_satisfaction_only_lifesat = df_satisfaction_only_lifesat.drop(columns=['freq', 'unit', 'isced11'])
+    df_satisfaction_only_lifesat = df_satisfaction_only_lifesat.rename(columns={"geo\\TIME_PERIOD": "country"})
+    return [df_satisfaction_only_lifesat, dataframes[1]]
+
+
+
+def main():
+    engine = prepare_pipeline_engine()
+    dataframes = get_dataframes_from_sources()
+    dataframes = process_dataframes(dataframes)
+    dataframes[0].to_sql("satisfaction", engine, index=False, if_exists="replace")
+    dataframes[1].to_sql("movies", engine, index=False, if_exists="replace")
+
+if __name__ == "__main__":
+    main()
